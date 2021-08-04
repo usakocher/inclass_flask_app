d30fdfeedc1f000edc504dd7911580d1b507895a
usakocher <adammkocher@gmail.com>
2021-08-04 12:07:41 -0400

    test changes

---
+++ grocery_inventory/authentication/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request
+from grocery_inventory.forms import UserLoginForm
+
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print([email, password])
+    return render_template('signup.html', form = form)
+
+@auth.route('/signin', methods = ['GET', 'POST'])
+def signin():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+    return render_template('signin.html', form = form)
